36eda831f77ce8f4f3fd66581f2723043b7c78f3
dikemtunc-cloud <dikem.tunc@gmail.com>
2026-09-15 20:57:31 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ const $=s=>document.querySelector(s);
 
 // Google Apps Script Web App endpoint. Paste the deployed /exec URL here after deployment.
 const DELIVERY_CONFIG={
-  webAppUrl:"https://script.google.com/macros/s/AKfycbxitv_tH996xx2V6e_ZZE2zXFLo0MFPxab4VX2zkzZMK5DeGNxqVlfMSWSxGY7bOM_V/exec"
+  webAppUrl:"https://script.google.com/macros/s/AKfycbx1sno8dbjjgdaV8P-znggJRIfXQ7RRUboVDaPI_XVKW6GE07R5Otb-98Ezbk5IvaSS-w/exec"
 };
 
 /* =========================================================
